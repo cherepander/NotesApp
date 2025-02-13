098e638d9567af1dd5de7d838265d920630f5428
cherepander <paueldups@gmail.com>
2025-02-14 00:57:02 +0300

    added icons, rounded corners

--- app/src/main/java/com/companyname/myapplication/ui/DragAndDropNoteList.kt
+++ app/src/main/java/com/companyname/myapplication/ui/DragAndDropNoteList.kt
@@ -19,11 +19,13 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -33,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
@@ -63,7 +66,8 @@ fun DragAndDropNoteList(
             Box(
                 modifier = modifier
                     .fillMaxWidth()
-                    .background(if (index == draggedNoteIndex) Color.LightGray else Color.Transparent)
+                    .padding(8.dp)
+                    .background(if (index == draggedNoteIndex) Color.LightGray else Color.White, shape = RoundedCornerShape(10.dp))
                     .pointerInput(Unit) {
                         detectDragGestures(
                             onDragStart = {
@@ -93,6 +97,7 @@ fun DragAndDropNoteList(
                         )
                     }
                     .padding(8.dp)
+
             ) {
 
                 Row(
--- app/src/main/java/com/companyname/myapplication/ui/NoteListScreen.kt
+++ app/src/main/java/com/companyname/myapplication/ui/NoteListScreen.kt
@@ -1,6 +1,8 @@
 package com.companyname.myapplication.ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -36,7 +38,7 @@ fun NoteListScreen(navController: NavController, viewModel: NoteViewModel) {
         },
         floatingActionButton = {
             FloatingActionButton(onClick = { navController.navigate("add_note") }) {
-                Text("+")
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
             }
         }
     ) { padding ->
